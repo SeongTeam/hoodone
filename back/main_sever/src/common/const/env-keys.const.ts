149ff192bc8537763cea68af42bd483e04e75eb5
Cori1304-Seong <160833798+Cori1304-Seong@users.noreply.github.com>
2024-02-28 17:49:32 +0900

    <feature>init db connect (#5)
    * main_server  BoilerPlate
    
    * <fix> SearchModule 제거
    
    * <feature> add typeorm.config & DB연결에 사용할 패키지
    
    * <feature> webpack 추가
    
    express에서 사용하는 nodemon과 동일하게 사용하는 hot Reload 패키지
    
    * <refactoring> posts module=> boards module
    
    * <feature> 프로젝트에서 사용할 기본 패키지 추가
    
    * <feature> 프로젝트 prettier 설정 초기화
    
    * <refactoring> common.entity.ts 삭제
    
    사용하지 않을 것으로 예상하기에 삭제
    
    * <feature> .env 사용 & config 로직 추가
    
    * <fix> DB 연동 로직 추가 conflicts  해결

---
+++ back/main_sever/src/common/const/env-keys.const.ts
@@ -0,0 +1,9 @@
+export const ENV_PROTOCOL_KEY = 'PROTOCOL'
+export const ENV_HOST_KEY = 'HOST'
+
+// DB 관련
+export const ENV_DB_HOST_KEY = 'DB_HOST'
+export const ENV_DB_PORT_KEY = 'DB_PORT'
+export const ENV_DB_USER_NAME_KEY = 'DB_USERNAME'
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD'
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE'
--- back/main_sever/src/configs/typeorm.config.ts
+++ back/main_sever/src/configs/typeorm.config.ts
@@ -1,27 +1,35 @@
-    // TODO: src/database/typeorm.config.ts
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+import { Injectable } from '@nestjs/common'
+import { BaseModel } from 'src/common/entity/base.entity'
+import { ConfigModule } from '@nestjs/config'
+import {
+  ENV_DB_DATABASE_KEY,
+  ENV_DB_HOST_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USER_NAME_KEY,
+} from 'src/common/const/env-keys.const'
 
-    import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
-    import { Injectable } from '@nestjs/common'
-    
-    @Injectable()
-    export class TypeormConfig implements TypeOrmOptionsFactory {
-      createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-          type: 'postgres',  // TODO: 데이터베이스 종류 
-          url: '',  // TODO: ex) postgresql://username:password@hostname:port/database
-          host: 'localhost', // TODO: 데이터베이스 서버 호스트
-          port: 5432, // TODO: 데이터베이스 포트
-          username: 'postgres',
-          password: 'postgres',
-          database: 'board-app', // TODO: 연결할 데이터베이스 이름
-          synchronize: true,  // TODO: 스키마 자동 동기화 (production에서는 false)
-          dropSchema: false, // TODO: 애플리케이션 실행시 기존 스키마 삭제 여부
-          keepConnectionAlive: true, // TODO: 애플리케이션 재시작 시 연결 유지
-          logging: true, // TODO: 데이터베이스 쿼리 로깅 여부
-          entities: [__dirname + '/../**/*.entity{.ts,.js}'],  //TODO: 중요! 엔티티 클래스 경로
-          extra: {
-            max: 100
-          }
-        } as TypeOrmModuleOptions
-      }
-    }
+@Injectable()
+export class TypeormConfig implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres', // 데이터베이스 종류
+      url: '', // TODO: ex) postgresql://username:password@hostname:port/database
+      host: process.env[ENV_DB_HOST_KEY],
+      port: +process.env[ENV_DB_PORT_KEY], // 데이터베이스 포트
+      username: process.env[ENV_DB_USER_NAME_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY], // 연결할 데이터베이스 이름
+      autoLoadEntities: true,
+      synchronize: true, // 스키마 자동 동기화 (production에서는 false)
+      dropSchema: true, // 애플리케이션 실행시 기존 스키마 삭제 여부
+      keepConnectionAlive: true, // 애플리케이션 재시작 시 연결 유지
+      logging: true, // TODO: 데이터베이스 쿼리 로깅 여부
+      entities: [BaseModel, __dirname + 'src/**/entity/*.entity.{ts,js}'], //엔티티 클래스 경로
+      extra: {
+        max: 100,
+      },
+    } as TypeOrmModuleOptions
+  }
+}
