e6304d5f7af75070086506c2fa84a788b83ef18f
Cori1304-Seong <andyjm7739@gmail.com>
2024-03-02 20:50:49 +0900

    <feature> JWT auth 로직 추가

--- back/main_sever/src/auth/auth.controller.ts
+++ back/main_sever/src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthCredentialsDto, RegisterUserDto } from './dto/auth-credential.dto'
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto, RegisterUserDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body(ValidationPipe) registerUserDto: RegisterUserDto): Promise<void> {
-    return this.authService.signUp(registerUserDto)
+  signUp(@Body(ValidationPipe) registerUserDto: RegisterUserDto) {
+    return this.authService.registerWithEmail(registerUserDto);
+  }
+
+  @Post('/login/email')
+  login(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.loginWithEmail(authCredentialsDto);
   }
 }
