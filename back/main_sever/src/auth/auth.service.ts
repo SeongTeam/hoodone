50e67cea7d97ac80ec2531153e0bc42725c1d68b
Cori1304-Seong <160833798+Cori1304-Seong@users.noreply.github.com>
2024-03-02 21:36:05 +0900

    <feature>JWT auth 로직 추가  (#9)
    * main_server  BoilerPlate
    
    * <feature> JWT auth 로직 추가
    
    * <feature> auth 서비스 추가로 인한 USER Module & 로직 변경
    
    * <refactoring> User entity 이름 수정 적용
    
    * <feature> auth관련 const env-key 추가
    
    * <feature> TypeormConfig 설정값 변동
    
    <변동사항>
    1. nest.js 실행시 마다 DB에 데이터가 삭제 되는 속성 사용안함 으로 변경
    2.  데이터베이스 쿼리 로깅 사용안함 으로 변경
    
    * <format> prettier 세미콜론 기능 ON
    
    * Delete back/main_sever/package-lock.json (#10)
    
    예기치 못한 오류로 인해서 lock.json을 삭제 후 다시 npm install을 했는데 
    실수로 commit이 되었습니다

--- back/main_sever/src/auth/auth.service.ts
+++ back/main_sever/src/auth/auth.service.ts
@@ -1,36 +1,103 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
-import { InjectRepository } from '@nestjs/typeorm'
-import { AuthCredentialsDto, RegisterUserDto } from './dto/auth-credential.dto'
-import { UserRepository } from 'src/users/user.repository'
-import * as bcrypt from 'bcrypt'
-import { ConfigService } from '@nestjs/config'
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthCredentialsDto, RegisterUserDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import {
+  ENV_AUTH_SALT2024_KEY,
+  ENV_HASH_ROUNDS_KEY,
+  ENV_JWT_SECRET_KEY,
+} from 'src/common/const/env-keys.const';
+import { User } from 'src/users/entity/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
-    @InjectRepository(UserRepository)
-    private userRepository: UserRepository,
+    private readonly usersService: UsersService,
     private readonly configService: ConfigService,
   ) {}
 
-  async signUp(registerUserDto: RegisterUserDto): Promise<void> {
-    return this.userRepository.createUser(registerUserDto)
+  // todo bcript를 사용하자 단 너무 느게리게 사용하지 말고
+  // salt를 잘 사용하고 주기적으로 salt를 바꿔주자
+
+  async authenticateWithEmailAndPassword(user: Pick<User, 'email' | 'password'>) {
+    /**
+     * 1. 사용자가 존재하는지 확인 (email)
+     * 2. 비밀번호가 맞는지 확인
+     * 3. 모두 통과되면 찾은 상용자 정보 반환
+     */
+    const existingUser = await this.usersService.getUserByEmail(user.email);
+
+    if (!existingUser) {
+      throw new UnauthorizedException('존재하지 않는 사용자입니다.');
+    }
+
+    /**
+     * 파라미터
+     *
+     * 1) 입력된 비밀번호
+     * 2) 기존 해시 (hash) -> 사용자 정보에 저장돼있는 hash
+     */
+
+    const passOk = await bcrypt.compare(
+      `${user.password}${this.configService.get<string>(ENV_AUTH_SALT2024_KEY)}`,
+      existingUser.password,
+    );
+    if (!passOk) {
+      throw new UnauthorizedException('비밀번호가 틀렸습니다.');
+    }
+
+    return existingUser;
+  }
+  loginUser(user: Pick<User, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
-    const { email, password } = authCredentialsDto
-    const user = await this.userRepository.findOneBy({ email: email })
+  async loginWithEmail(user: AuthCredentialsDto): Promise<{ accessToken: string }> {
+    const existingUser = await this.authenticateWithEmailAndPassword(user);
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // 유저 토큰 생성 ( Secret + Payload )
-      const payload = { email }
-      const accessToken = await this.jwtService.sign(payload)
+    return this.loginUser(existingUser);
+  }
 
-      return { accessToken }
-    } else {
-      throw new UnauthorizedException('login failed')
-    }
+  /**
+   * jwt Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub -> id
+   * 3) type : 'access' | 'refresh'
+   *
+   * {email: string, id: number}
+   */
+  signToken(user: Pick<User, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>(ENV_JWT_SECRET_KEY),
+      // seconds
+      expiresIn: isRefreshToken ? 3600 : 300, // 토큰 타입에 따라 기간을 다르게 설정
+    });
+  }
+
+  async registerWithEmail(user: RegisterUserDto) {
+    const hash = await bcrypt.hash(
+      `${user.password}${this.configService.get<string>(ENV_AUTH_SALT2024_KEY)}`,
+      parseInt(this.configService.get<string>(ENV_HASH_ROUNDS_KEY)),
+    );
+
+    const newUser = await this.usersService.createUser({
+      ...user,
+      password: hash,
+    });
+
+    return this.loginUser(newUser);
   }
 }
--- back/main_sever/src/users/users.service.ts
+++ back/main_sever/src/users/users.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { QueryRunner, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  getUsersRepository(qr?: QueryRunner) {
+    return qr ? qr.manager.getRepository<User>(User) : this.usersRepository;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async createUser(user: Pick<User, 'email' | 'nickname' | 'password'>) {
+    // 1) nickname 중복이 없는지 확인
+    // exist() -> 만약에 조건에 해당되는 값이 있으면 true 반환
+    const nicknameExists = await this.usersRepository.exist({
+      where: {
+        nickname: user.nickname,
+      },
+    });
+
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다!');
+    }
+
+    const emailExists = await this.usersRepository.exist({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (emailExists) {
+      throw new BadRequestException('이미 가입한 이메일입니다!');
+    }
+
+    const userObject = this.usersRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+    const newUser = await this.usersRepository.save(userObject);
+    return newUser;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async getAllUsers() {
+    return this.usersRepository.find();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getUserByEmail(email: string) {
+    return this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 }
