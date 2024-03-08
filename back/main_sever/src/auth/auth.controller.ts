60c72aec4c695329633dc8ee54a3bea008e032e8
Cori1304-Seong <andyjm7739@gmail.com>
2024-03-08 19:23:23 +0900

    <feature> JWT 토큰 재발급 API 추가

--- back/main_sever/src/auth/auth.controller.ts
+++ back/main_sever/src/auth/auth.controller.ts
@@ -1,10 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
+import { Controller,  Post, Headers, Body, ValidationPipe, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto, RegisterUserDto } from './dto/auth-credential.dto';
+import { RefreshTokenGuard } from './guard/bearer-token.guard';
+import { BasicTokenGuard } from './guard/basic-token.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('token/access')
+  @UseGuards(RefreshTokenGuard)
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+
+    /**
+     * {accessToken: {token}}
+     */
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  @UseGuards(RefreshTokenGuard)
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    console.log(`token ===>> ${token}`)
+
+    const newToken = this.authService.rotateToken(token, true);
+
+    /**
+     * {refreshToken: {token}}
+     */
+    return {
+      refreshToken: newToken,
+    };
+  }
 
   @Post('/signup')
   signUp(@Body(ValidationPipe) registerUserDto: RegisterUserDto) {
@@ -12,7 +46,13 @@ export class AuthController {
   }
 
   @Post('/login/email')
-  login(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
-    return this.authService.loginWithEmail(authCredentialsDto);
+  @UseGuards(BasicTokenGuard)
+    login(  @Headers('authorization') rawToken: string,
+    ) {
+      const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+      const credentials:AuthCredentialsDto = this.authService.decodeBasicToken(token);
+  
+    return this.authService.loginWithEmail(credentials);
   }
 }
