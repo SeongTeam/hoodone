231fc96adfc6d240717a95588f87c330d89a79bc
Cori1304-Seong <andyjm7739@gmail.com>
2024-03-19 20:21:43 +0900

    <feature>Add posts entity
    1. post에 대한 entitiy를 추가
    2. 잘못 적혀 있던 post entitiy 이름 변경

---
+++ back/main_sever/src/posts/entities/posts.entity.ts
@@ -0,0 +1,40 @@
+import { IsString } from 'class-validator';
+import { BaseModel } from 'src/common/entity/base.entity';
+import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { UserModel } from 'src/users/entities/user.entity';
+
+@Entity()
+export class PostsModel extends BaseModel {
+  // 1) UsersModel과 연동한다 Foreign Key를 이용해서
+  // 2) null이 될 수 없다.
+  //
+  @ManyToOne(() => UserModel, (user) => user.posts, {
+      // nullable: false,
+  })
+  author: UserModel;  
+
+  @Column()
+  @IsString({
+    message: stringValidationMessage,
+  })
+  title: string;
+
+  @Column()
+  @IsString({
+    message: stringValidationMessage,
+  })
+  content: string;
+
+  @Column()
+  likeCount: number;
+
+  @Column()
+  commentCount: number;
+
+  @Column()
+  isPublished: boolean
+  
+
+  
+}
