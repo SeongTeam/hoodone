07822f5564d8c463a9ec0f4a4ba1f681ca1a31d7
Cori1304-Seong <andyjm7739@gmail.com>
2024-03-19 20:30:42 +0900

    <feature> PostModel import 객체 추가

--- back/main_sever/src/posts/posts.module.ts
+++ back/main_sever/src/posts/posts.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { CommonModule } from 'src/common/common.module';
+
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-
+import { PostsModel } from './entities/posts.entity';
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      PostsModel,
+    ]),
+    AuthModule,
+    UsersModule,
+    CommonModule,
+  ],
   controllers: [PostsController],
+  
   providers: [PostsService],
 })
 export class PostsModule {}
