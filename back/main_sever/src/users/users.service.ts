921fd6ad9899a140269cbd2736308b62f2fa5871
Cori1304-Seong <andyjm7739@gmail.com>
2024-03-26 14:43:58 +0900

    <feature> find로직  user table 디폴드 값 추가

--- back/main_sever/src/users/users.service.ts
+++ back/main_sever/src/users/users.service.ts
@@ -1,7 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserModel } from './entities/user.entity';
+import { UserModel, UserModelStatus } from './entities/user.entity';
 import { QueryRunner, Repository } from 'typeorm';
+import {
+  DEFAULT_CREATE_UserModel_OPTIONS,
+  DEFAULT_FIND_UserModel_OPTIONS,
+} from './const/default_qurey_user_options';
 
 @Injectable()
 export class UsersService {
@@ -17,7 +21,7 @@ export class UsersService {
   async createUser(user: Pick<UserModel, 'email' | 'nickname' | 'password'>) {
     // 1) nickname 중복이 없는지 확인
     // exist() -> 만약에 조건에 해당되는 값이 있으면 true 반환
-    const nicknameExists = await this.usersRepository.exist({
+    const nicknameExists = await this.usersRepository.exists({
       where: {
         nickname: user.nickname,
       },
@@ -27,7 +31,7 @@ export class UsersService {
       throw new BadRequestException('이미 존재하는 nickname 입니다!');
     }
 
-    const emailExists = await this.usersRepository.exist({
+    const emailExists = await this.usersRepository.exists({
       where: {
         email: user.email,
       },
@@ -41,13 +45,16 @@ export class UsersService {
       nickname: user.nickname,
       email: user.email,
       password: user.password,
+      ...DEFAULT_CREATE_UserModel_OPTIONS,
     });
     const newUser = await this.usersRepository.save(userObject);
     return newUser;
   }
 
   async getAllUsers() {
-    return this.usersRepository.find();
+    return this.usersRepository.find({
+      ...DEFAULT_FIND_UserModel_OPTIONS,
+    });
   }
 
   async getUserByEmail(email: string) {
