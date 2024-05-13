306ef800643f5ef7dbdfd242d8bb7cbc32f8a227
Cori1304-Seong <andyjm7739@gmail.com>
2024-05-13 17:34:36 +0900

    <feature>Change response property

--- front/src/app/api/auth/signUp/route.ts
+++ front/src/app/api/auth/signUp/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
             message: `POST signUP 호출 성공 ${nickname + email + password}`,
         });
 
-        const response: Response = await fetch(`${backURL}/auth/signup`, {
+        const res: Response = await fetch(`${backURL}/auth/signup`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -27,28 +27,43 @@ export async function POST(req: NextRequest) {
             next: { revalidate: 60 }, // R
         });
 
-        if (response.ok) {
-            const responseData: AuthApiResponseDto = await response.json();
-            logger.info('Backend Response', { message: response.text() });
+        if (res.ok) {
+            const { email, nickname } = await res.json();
 
-            const { email, nickname } = responseData.postSignup!;
+            logger.info('Backend Response', { message: email + '-' + nickname });
             return NextResponse.json({
                 body: {
                     title: 'SignUp successful ',
-                    status: response.status,
+                    status: res.status,
                     data: { nickname: nickname, email: email },
                 },
             });
         } else {
-            logger.error('elsee Backend Error', { message: `${(await response.json()).response}` });
-            return NextResponse.json({ error: 'Sign up failed' }, { status: 500 });
+            const errorData = await res.json();
+            const body = res.body;
+            const { message, error, errorCode, detail } = errorData; // err일때 사용하는 프로퍼티 정해서 return 하자
+            logger.error('elsee Backend Error', {
+                message: `${detail.message}/MSG + [${detail.error}] ${error}`,
+            });
+            // TransactionInterceptor에서 .error에 집어 넣은 메세지가 error에 담겨 졌다.
+            logger.error('elsee Backend Error response =>', { reponse: `${detail}` });
+
+            // body는 담기지 않고 error는 담기고 있다. => error라는 프로퍼티를 이용해서 Exception message를 사용하자
+            return NextResponse.json(
+                {
+                    error: `Sign up failed - ${detail.error}`,
+                    message: detail.message,
+                    pst: detail.pst,
+                }, // signUp.tsx에서 error의 있는 string을 읽는것이 필수
+                { status: res.status },
+            );
         }
-    } catch (error) {
+    } catch (error: any) {
         logger.error('catch err Internal Server Error', { message: error });
 
         return NextResponse.json(
-            { error: 'Signout failed. please check email and password', message: error },
-            { status: 200 },
+            { error: 'Sign up failed. please check email and password', message: error },
+            { status: 500 },
         );
     }
 }
