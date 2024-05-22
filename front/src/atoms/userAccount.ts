5ce1788b862ce96455b6ed7bb97745d5c4b68ba2
armcortex3445 <wlghks5966@naver.com>
2024-05-22 12:21:43 +0900

    <feature> Implement log-out logic

--- front/src/atoms/userAccount.ts
+++ front/src/atoms/userAccount.ts
@@ -11,7 +11,7 @@ export interface userAccountState {
     profileImg: string;
 }
 
-const defaultUserAccount: userAccountState = {
+export const defaultUserAccount: userAccountState = {
     email: '',
     nickName: '',
     isLogin: false,
