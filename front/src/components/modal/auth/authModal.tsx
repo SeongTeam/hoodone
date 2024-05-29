7b5afe18fdd14761f97f0a7ea30798d5c8ffd89a
Cori1304-Seong <andyjm7739@gmail.com>
2024-05-29 23:02:27 +0900

    <refactoring> delete space

--- front/src/components/modal/auth/authModal.tsx
+++ front/src/components/modal/auth/authModal.tsx
@@ -10,7 +10,6 @@ import {
     ModalOverlay,
     useDisclosure,
     Text,
-
 } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
@@ -24,7 +23,6 @@ import { customColors } from '@/utils/chakra/customColors';
 - accessToken 만료까지 User 로그인 상태 유지하도록 수정하기
   - 새로 고침시, 로그인 아이콘 변경되는 이슈 해결하기
 */
-  
 
 const AuthModal: React.FC = () => {
     //const { isOpen, onOpen, onClose } = useDisclosure();
