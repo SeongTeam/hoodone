4f9f31da52ceb7f65b85f5b3a78e1b7a957013df
Cori1304-Seong <andyjm7739@gmail.com>
2024-05-31 23:27:50 +0900

    <refactoring> Change passwordValidationMessage

--- back/main_sever/src/auth/dto/register-user.dto.ts
+++ back/main_sever/src/auth/dto/register-user.dto.ts
@@ -1,6 +1,7 @@
 import { PickType } from '@nestjs/mapped-types';
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { passwordValidationMessage } from 'src/common/validation-message/password-vaildation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { UserModel } from 'src/users/entities/user.entity';
 
@@ -18,7 +19,7 @@ export class RegisterUserDto extends PickType(UserModel, ['nickname', 'email', '
     @MaxLength(20)
     //영어, 숫자, 특수문자 각각 1개씩 필요 가능한 유효성 체크
     @Matches(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{3,}$/, {
-        message: 'password only accepts english and number',
+        message: passwordValidationMessage,
     })
     password: string;
 }
