00dc5d0b58dc24a47153b16e1cccd46f95f67e56
Cori1304-Seong <andyjm7739@gmail.com>
2024-06-03 14:11:33 +0900

    <feature> Add getCompareTempUserPinCode & resetPassword DTOs

--- shared/response-dto/auth-api-response.dto.ts
+++ shared/response-dto/auth-api-response.dto.ts
@@ -16,9 +16,17 @@ export class AuthApiResponseDto {
     //   postSendPinCode 는 이런 형식으로 return response: '250 2.0.0 OK  1716551382 d2e1a72fcca58-6f8fcbea886sm952420b3a.137 - gsmtp',
     sendSignUpPinCode?: string;
 
+    sendPasswordResetLink?: string;
+
     getCompareTempUserPinCode?: {
         statusCode: Number;
         message: string;
         result: boolean;
+    };
+
+    resetPassword?: {
+        generatedMaps: [];
+        raw: [];
+        affected: 1;
     };
 }
