14103c3075d40dd6a708d525f5e92ea80b24d617
armcortex3445 <wlghks5966@naver.com>
2024-06-05 23:40:27 +0900

    <fix> fix infinite scroll bug
    - add clean up function on useEffect to reset scroll condition
    - seperate useEffect to invoke clean up function at component unmount
      once
    - change global varaiable to state for preventing variable from being
      accssed by other components

--- front/src/components/posts/view/loadMorePosts.tsx
+++ front/src/components/posts/view/loadMorePosts.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import PostList from "./postList";
 import { PostType } from "@/atoms/post";
+import { off } from "process";
 
 /*TODO
 - route handler GET METHOD cache 활용 여부 확인
@@ -19,8 +20,7 @@ import { PostType } from "@/atoms/post";
 
 
 
-let offset = 2;
-let isLoadingEnable= true;
+const INITIAL_OFFSET = 2;
 const getPaginatedPostsFromAPI = async (offset: number) => {
     const res = await fetch(`http://localhost:4000/api/posts?offset=${offset}`)
     const posts = res.json();
@@ -30,31 +30,69 @@ const getPaginatedPostsFromAPI = async (offset: number) => {
 const LoadMorePosts : React.FC = ( ) => {
     const { ref, inView } = useInView()
     const [ posts, setPosts ] = useState<PostType[]>([])
-    
-    // execute useEffect when ref( Element ) is in view
-    useEffect(() => {
-        if(inView && isLoadingEnable) {
-            getPaginatedPostsFromAPI(offset++).then((res) => {
-                setPosts( (prev) => ([...prev, ...res]));
-                if(res.length <= 0 ) isLoadingEnable = false;
-            })
+    const [ isLoading, setIsLoading ] = useState<boolean>(false);
+    const [ hasMore, setHasMore ] = useState<boolean>(true);
+    const [ offset, setOffset ] = useState<number>(INITIAL_OFFSET);
 
-            }
-           
-        }, [inView,posts])
+    console.log("inView", inView);
+    console.log("hasMore", hasMore);
+
+    const loadPosts = useCallback( async () => {
+        if(!hasMore|| isLoading) {
+            console.log("no more posts",hasMore,isLoading);
+            return;
+        }
+        setIsLoading(true);
+        try{
+            console.log("try state");
+            const newPosts = await getPaginatedPostsFromAPI(offset);
+            setPosts(posts => [...posts, ...newPosts]);
+            setOffset(prev => prev++);
+            if(newPosts.length <= 0) setHasMore(true);
+        }
+        catch(error){
+            console.log(error);
+            setHasMore(false);
+        }
+        finally{
+            console.log("finally state");
+            setIsLoading(false);
+        }
+    },[isLoading,hasMore,offset]);
+    
+
+    // execute useEffect when ref( Element ) is in view
+
+    useEffect( () => {
+
+        return () => {
+            console.log("clean up");
+            setOffset(INITIAL_OFFSET);
+            setHasMore(true);
+        };
+    },[]);
+
+    useEffect( () => {
+        if(inView){
+            console.log("inView");
+            loadPosts();
+        }
+    },[inView,loadPosts])
+
+
 
     return (
         <>
-        <PostList postList={posts}/>
+            <PostList postList={posts}/>
             <section>
                 <div ref={ref}>
-                    <Image
+                    {isLoading && <Image
                         src="/hood1/spinner.svg"
                         alt="spinner"
                         width={100}
                         height={100}
                         className="object-contain"
-                    />
+                    />}
                 </div>
             </section>
         </>
