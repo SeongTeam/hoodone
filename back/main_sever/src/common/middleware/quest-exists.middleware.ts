dfb402bdc92c361bc4cca283ce100d7a341cfe7d
Cori1304-Seong <andyjm7739@gmail.com>
2024-06-20 01:12:35 +0900

    PostExistMiddleware을 quest와 sb로 나눔

---
+++ back/main_sever/src/common/middleware/quest-exists.middleware.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { QuestPostsService } from 'src/posts/-quest/quest_post.service';
+
+@Injectable()
+export class QuestExistsMiddelware implements NestMiddleware {
+    constructor(private readonly postService: QuestPostsService) {}
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const postId = req.params.postId;
+
+        if (!postId) {
+            throw new BadRequestException('QuestPost ID 파라미터는 필수입니다.');
+        }
+
+        const exists = await this.postService.hasExistedId(parseInt(postId));
+
+        if (!exists) {
+            throw new BadRequestException('QuestPost가 존재하지 않습니다.');
+        }
+
+        next();
+    }
+}
