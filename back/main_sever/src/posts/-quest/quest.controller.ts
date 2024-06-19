b21b77137f58aa57ba488bc77cd44e322ed1b112
Cori1304-Seong <andyjm7739@gmail.com>
2024-06-20 00:22:59 +0900

    QuestPost controller와 service추가

---
+++ back/main_sever/src/posts/-quest/quest.controller.ts
@@ -0,0 +1,154 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Headers,
+    Param,
+    Patch,
+    Post,
+    Query,
+    UseGuards,
+    UseInterceptors,
+} from '@nestjs/common/decorators';
+import { PostsUseCases } from '../usecase/post.use-case';
+import { BoardUseCase } from 'src/boards/usecase/board.use-case';
+import { User } from 'src/users/decorator/user.decorator';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
+import { AccessTokenGuard } from 'src/auth/guard/token/access-token.guard';
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { PostApiResponseDto } from 'hoodone-shared/dist/response-dto/post-api-reponse.dto';
+import { GetPaginatedPostsQueryDTO } from '../dto/get-paginated-posts.dto';
+import { IsPublic } from 'src/common/decorator/is-public.decorator';
+import { ParseIntPipe, ValidationPipe } from '@nestjs/common/pipes';
+import { Roles } from 'src/users/decorator/roles.decorator';
+import { RoleType } from 'src/users/const/role.type';
+import { QuestPostOwnerGuard } from '../guard/quest-post-owner.guard';
+import { RoleGuard } from 'src/auth/guard/role.guard';
+import { UpdatePostDto } from '../dto/update-post.dto';
+
+/*TODO
+- Comment list 미포함하여 반환하도록 수정
+    - front의 infinite scroll 동작시 fetch를 감소시켜야하므로 수정 필요
+*/
+@Controller('quests')
+export class QuestPostsController {
+    constructor(
+        private readonly postUseCase: PostsUseCases,
+        private readonly boardUseCase: BoardUseCase,
+    ) {}
+
+    /*TODO
+    - Image URL 저장 추가하기
+    */
+    @Post()
+    @UseGuards(AccessTokenGuard)
+    @UseInterceptors(TransactionInterceptor)
+    async post(@User('id') userId: number, @Body() body: CreatePostDto, @QueryRunner() qr: QR) {
+        // 로직 실행
+        console.log(body);
+        const newPost = await this.postUseCase.createQuest(userId, body, qr);
+
+        return newPost;
+    }
+
+    @Get('/all')
+    async getAll() {
+        const res = new PostApiResponseDto();
+        res.getAll = await this.postUseCase.getAllQuests();
+        return res;
+    }
+
+    /** TODO paginated 구현 quest와 sb 나눌 것인지 논의*/
+    @Get('/paginated')
+    async getPaginatedPosts(
+        @Query(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }))
+        queryParams: GetPaginatedPostsQueryDTO,
+    ) {
+        const { offset, limit } = queryParams;
+        const res = new PostApiResponseDto();
+        res.getPaginatedPosts = await this.postUseCase.getPaginatedPosts(offset, limit);
+
+        return res;
+    }
+
+    // TODO 복수와 단수를 반환하는 API를 만들고
+    // 복수를 반환할때 Query string을 사용하는 로직 추가
+    @Get()
+    async getQuestsByEmail(@Query('email') userEmail: string) {
+        let res = new PostApiResponseDto();
+        res.getAll = await this.postUseCase.getQuestsByEmail(userEmail);
+
+        return res;
+    }
+
+    @Get('/:id')
+    @IsPublic()
+    async getById(@Param('id', ParseIntPipe) id: number) {
+        let res = new PostApiResponseDto();
+        res.getById = await this.postUseCase.getQuestById(id);
+
+        return res;
+    }
+
+    @Patch('/:id')
+    @Roles(RoleType.USER, RoleType.ADMIN)
+    @UseGuards(AccessTokenGuard, QuestPostOwnerGuard, RoleGuard)
+    @UseInterceptors(TransactionInterceptor)
+    async patch(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: UpdatePostDto,
+        @QueryRunner() qr: QR,
+    ) {
+        return this.postUseCase.updateQuest(id, body);
+    }
+
+    @Delete('/quest:id')
+    @Roles(RoleType.USER, RoleType.ADMIN)
+    @UseGuards(AccessTokenGuard, QuestPostOwnerGuard, RoleGuard)
+    @UseInterceptors(TransactionInterceptor)
+    deleteQuest(@Param('id', ParseIntPipe) id: number, @QueryRunner() qr: QR) {
+        return this.postUseCase.deleteQuest(id, qr);
+    }
+
+    /*TODO
+    - 프로토타입 로직 유지 혹은 다른 함수에 병합 고려
+    */
+    @Get('/board/:boardId')
+    async getPostsByBoardId(@Param('boardId', ParseIntPipe) boardId: number) {
+        return await this.postUseCase.getPostFromBoard(boardId, 1, 10);
+    }
+
+    @Post('/board/:boardId')
+    @Roles(RoleType.USER, RoleType.ADMIN)
+    @UseGuards(AccessTokenGuard, RoleGuard)
+    @UseInterceptors(TransactionInterceptor)
+    async addPost(
+        @User('id') userId: number,
+        @Param('boardId', ParseIntPipe) boardId: number,
+        @Body() body: CreatePostDto,
+        @QueryRunner() qr: QR,
+    ) {
+        /*TODO
+        - Post 생성시, boardID로 설정하도록 생성로직 변경필요
+        - post 생성 후, board에서 생성된 postid 추가 필요
+        */
+        //await this.boardUseCase.addPost(boardId, newPost.id, qr);
+    }
+
+    @Patch('/board/migration')
+    @Roles(RoleType.USER, RoleType.ADMIN)
+    @UseGuards(AccessTokenGuard, RoleGuard)
+    @UseInterceptors(TransactionInterceptor)
+    async patchBoardMigration(@Body() body, @QueryRunner() qr: QR) {
+        const { boardIdList, postIdList } = body;
+        /*TODO
+        - 특정 post 리스트를 board에 옮기는 로직 구현 필요.
+        - srcboard 에서 dstBoard로 옮기는가
+        - 서로다른 board에있는 postf를 특정 dstBoard로 옮기는가
+        필요하다면, 여러개 작성 필요
+        */
+    }
+}
