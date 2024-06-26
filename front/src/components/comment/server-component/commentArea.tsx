fb13b2d1343e38a94a0ae9707a1c76905996272d
Cori1304-Seong <andyjm7739@gmail.com>
2024-06-26 21:02:44 +0900

    quest Detail page init

--- front/src/components/comment/server-component/commentArea.tsx
+++ front/src/components/comment/server-component/commentArea.tsx
@@ -1,39 +1,33 @@
-import InputComment from "../InputComment";
-import { Box, Flex, Spinner  } from "@chakra-ui/react";
-import { Suspense } from "react";
-import RootCommentItemList from "./rootCommentItemList";
-
+import InputComment from '../InputComment';
+import { Box, Flex, Spinner, Stack, VStack } from '@chakra-ui/react';
+import { Suspense } from 'react';
+import RootCommentItemList from './rootCommentItemList';
 
 type CommentAreaProps = {
-    postID : number,
-    rootCommentID? : number,
-}
+    postID: number;
+    rootCommentID?: number;
+};
 
 /*TODO
 - commentlist(replylist) 가져올때, pagination 적용하여 부분적으로 가져오기
 */
-const CommentArea: React.FC<CommentAreaProps> = async ({postID, rootCommentID}) => {
-    
+const CommentArea: React.FC<CommentAreaProps> = ({ postID, rootCommentID }) => {
     const isCommentsPage = rootCommentID === undefined;
 
     return (
         <Box maxW="100%" overflow="hidden">
-            <Flex w="full" h="full" flexDirection={"column"} gap="1rem">
-                { isCommentsPage && <InputComment/>}
-                <Suspense fallback={<LoadingCommentList/>}>
-                    <RootCommentItemList postID={postID} rootCommentID={rootCommentID}/>
+            <VStack w="100%" h="full" flexDirection={'column'} gap="1rem" bg="yellow">
+                {isCommentsPage && <InputComment />}
+                <Suspense fallback={<LoadingCommentList />}>
+                    <RootCommentItemList postID={postID} rootCommentID={rootCommentID} />
                 </Suspense>
-            </Flex>
+            </VStack>
         </Box>
-    )
-}
+    );
+};
 
 export default CommentArea;
 
-
-const LoadingCommentList : React.FC = () => {
-
-    return (
-        <Spinner />
-    );
-}
+const LoadingCommentList: React.FC = () => {
+    return <Spinner />;
+};
