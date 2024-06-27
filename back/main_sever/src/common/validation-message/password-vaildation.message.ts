958a09743ed70f315ce6b5b8a8103a71b7cd9b6d
armcortex3445 <wlghks5966@naver.com>
2024-06-27 15:16:44 +0900

    <fix>[back] Modify Sign Up logic consistency
    - Fix error message to notify what problem in password
    - Fix regrex character length limit for consistent with @MinLength() and
      @MaxLength

--- back/main_sever/src/common/validation-message/password-vaildation.message.ts
+++ back/main_sever/src/common/validation-message/password-vaildation.message.ts
@@ -2,5 +2,5 @@ import { ValidationArguments } from 'class-validator';
 
 export const passwordValidationMessage = (args: ValidationArguments) => {
     // return `비밀번호는 영어와 숫자만 입력가능 합니다!`;
-    return 'password only accepts english, number, !@#$%^*+=-';
+    return 'password only accepts english, number, !@#$%^*+=-, and contains at least 1 character for each';
 };
