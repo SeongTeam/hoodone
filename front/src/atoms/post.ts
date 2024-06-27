4e469c18b5051903ab8843c74d06cea3379f3b7e
armcortex3445 <wlghks5966@naver.com>
2024-06-27 16:41:59 +0900

    <fix>[front] Modify postType
    - rename imageSrc property
    - add Interface of Quest and Submissions post
    - modify AuthorType included in postType

--- front/src/atoms/post.ts
+++ front/src/atoms/post.ts
@@ -1,12 +1,10 @@
 import { atom } from 'recoil';
 
 export type AuthorType = {
-    email: string;
     nickname: string;
-    profileImg: string;
+    profileImagePublicId: string;
 };
 
-type commentType = {};
 export type PostType = {
     id: number;
     createdAt: Date;
@@ -14,34 +12,34 @@ export type PostType = {
     deletedAt: Date | null;
     title: string;
     content: string;
-    likeCount: number;
-    commentCount: number;
+    cloudinaryPublicId?: string;
+    tags: string[];
+    favoriteCount: number;
     isPublished: boolean;
+    boardId: number;
     author: AuthorType;
-    thumbnailPublicID?: string;
-    thumbnailURL?: string;
-    linkedPostURL?: string;
-};
-
-export type PostLikeType = {
-    id: string;
-    postId: string;
-    voteValue: number;
+    comments: string[];
 };
 
 interface PostState {
     selectedPost: PostType | null;
     posts: PostType[];
-    postLikes: PostLikeType[];
 }
 
 const defaultPostState: PostState = {
     selectedPost: null,
     posts: [],
-    postLikes: [],
 };
 
 export const postState = atom<PostState>({
     key: 'PostState',
     default: defaultPostState,
 });
+
+export interface QuestInterface extends PostType {
+    sbPosts: string[];
+}
+
+export interface SubmissionInterface extends PostType {
+    parentPost: string;
+}
