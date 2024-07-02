024b060b10804acaf90f26b4ec868c4a4d799494
armcortex3445 <wlghks5966@naver.com>
2024-07-02 19:00:09 +0900

    <feature> rewrite post and image upload logic
    - add image upload lib to use cloudinary lib
    - rewrite post upload logic by branching post type

--- front/src/server-actions/postsActions.ts
+++ front/src/server-actions/postsActions.ts
@@ -1,14 +1,14 @@
 'use server';
-import { v2 as cloudinary } from 'cloudinary';
 import logger from '@/utils/log/logger';
 import { NextResponse } from 'next/server';
-import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import { PostType } from '@/atoms/post';
+import { PostType, NEW_POST_FORMAT, POST_TYPE, NewPostFormType } from '@/type/postType';
 import { revalidateTag } from 'next/cache';
-import { assert } from 'console';
+import { assert, log } from 'console';
 import { validateAuth } from '@/lib/server-only/authLib';
+import { getUserBasicInfo } from '@/lib/server-only/authLib';
+import { validateImage, uploadQuestImage, uploadSubmissionImage } from '@/utils/cloudinary/lib';
 
 /*
 ref : https://www.youtube.com/watch?v=5L5YoFm1obk
@@ -21,128 +21,130 @@ ref : https://www.youtube.com/watch?v=5L5YoFm1obk
 - 삭제 로직 구현하기
 */
 
-const configCld = {
-    secure: true,
-};
-
-cloudinary.config({
-    ...configCld,
-});
-
-type UploadResult = UploadApiResponse | UploadApiErrorResponse;
-
 const backendURL = process.env.BACKEND_URL;
-
-async function uploadThumbnail(ImageFile: File) {
-    /*TODO
-  - upload_stream 에러 발생 예외처리 추가 
-  */
-    try {
-        const arrayBuffer = await ImageFile.arrayBuffer();
-        const buffer = new Uint8Array(arrayBuffer);
-        const uploadResult: UploadResult = await new Promise((resolve, reject) => {
-            cloudinary.uploader
-                .upload_stream({}, (error, result) => {
-                    if (error) {
-                        reject(error as UploadApiErrorResponse);
-                        return;
-                    }
-                    resolve(result as UploadApiResponse);
-                })
-                .end(buffer);
-        });
-
-        if ('error' in uploadResult) {
-            logger.error('Cloudinary upload error', { message: uploadResult });
-            new Error('Cloudinary upload error');
-        }
-
-        return uploadResult;
-    } catch (error) {
-        logger.error('cloudinary.config(): ', cloudinary.config());
-        logger.error('uploadThumbnail error', { message: error });
-        return null;
-    }
-}
 
 export async function createPosts(formData: FormData) {
     /*TODO 
-    - NickName으로 변경 필요
-    - Date() 시간 기준은 서버 응답 기준인가, 클라이언트측의 앱 실행 기준인가 확인필요
-              - image upload 로직 구현
-    - clouldinray의 public id DB에 저장하기
+    - tag : string[] 타입 전송 구현하기
     - upload 실패 예외처리 로직 구현
       - 발생한 에러 명확한 정보 logger로 출력
       - 예외 상황에 따른 시나리오 고려 및 구현
+    - 테스트 진행하기
+        - 테스트 진행을 위해서 login 로직필요
+        - 
   */
-    try {
-        const title = formData.get('title') as string;
-        const content = formData.get('content') as string;
-        const isQuest = formData.get('isQuest') as string;
-        // const imgUrl = formData.get('imgUrl') as string;
-        // console.log(`${title} ${content} ${isQuest}`);
-        logger.error(`${title} ${content} ${isQuest}`);
+    const postDtoString = formData.get(NEW_POST_FORMAT.POST_DTO);
+    const imageFile = formData.get(NEW_POST_FORMAT.IMAGE) as File;
+    let res: Response | null = null;
 
-        assert(title && title.length > 0, 'title should be not empty');
-        assert(content && content.length > 0, 'content should be not empty');
-        // assert(imgUrl && imgUrl.length > 0, 'imgUrl should be not empty');
+    try {
+        if (typeof postDtoString !== 'string') {
+            logger.error('[createPosts] postDtoString is not string', { postDtoString });
+            throw new Error('[createPosts] postDtoString error');
+        }
+
+        logger.info('[createPosts] postDtoString: ', { postDtoString });
+
+        const postDto: NewPostFormType = JSON.parse(postDtoString);
 
         const newPost = {
-            title,
-            content,
+            title: postDto.title,
+            content: postDto.content,
+            tags: postDto.tags,
         } as PostType;
+        const accessToken = await validateAuth();
+        const type = postDto.type;
 
-        /**
-         * TODO imgUrl 로직을 추가해야 합니다, 아니면 DB에 imgUrl을 null able로 두어야 합니다
-         * 임시로 imgUrl은 아래 주소를 사용해서 보내겠습니다
-         *  mockUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpmk3ku78NmHbSKh60W0lHLovIBNPCWAwabw&s"
-         */
-        const mockUrl =
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpmk3ku78NmHbSKh60W0lHLovIBNPCWAwabw&s';
-        newPost.imgUrl = mockUrl;
-        const Image = formData.get('image') as File;
-        if (Image && Image.size > 0) {
-            const uploadResult = await uploadThumbnail(Image);
-            newPost.thumbnailPublicID = uploadResult?.public_id;
-            newPost.imgUrl = uploadResult?.public_id;
+        if (validateImage(imageFile)) {
+            let publicId: string | null = null;
+            const userInfo = await getUserBasicInfo();
+            if (!userInfo) {
+                logger.error('[createPosts] getUserBasicInfo fail ', { userInfo });
+                throw new Error('[createPosts] getUserBasicInfo error');
+            }
+
+            switch (type) {
+                case POST_TYPE.QUEST:
+                    publicId = await uploadQuestImage(imageFile, userInfo.email);
+                    break;
+                case POST_TYPE.SB:
+                    publicId = await uploadSubmissionImage(imageFile, userInfo.email);
+                    break;
+                default:
+                    throw new Error('[createPosts] POST_TYPE error');
+                    break;
+            }
+
+            if (publicId) newPost.cloudinaryPublicId = publicId;
+            else throw new Error('[createPosts] uploadQuestImage error');
         }
         /*TODO
         - token get 실패시, 홈이동 > 로그인 창 오픈. 
         */
-        const accessToken = await validateAuth();
-        let res: Response;
 
-        if (isQuest) {
-            res = await fetch(`${backendURL}/quests`, {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                    authorization: `Bearer ${accessToken}`,
-                },
-                body: JSON.stringify(newPost),
-            });
-        } else {
-            res = await fetch(`${backendURL}/sbs`, {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                    authorization: `Bearer ${accessToken}`,
-                },
-                body: JSON.stringify(newPost),
-            });
+        switch (type) {
+            case POST_TYPE.QUEST:
+                res = await formQuest(accessToken, newPost);
+                break;
+            case POST_TYPE.SB:
+                res = await formSubmissions(accessToken, newPost);
+                break;
+            default:
+                logger.error('[createPosts] POST_TYPE is invalid. type = ', type);
+                throw new Error('[createPosts] POST_TYPE error');
+                break;
         }
 
         if (!res.ok) {
             logger.error('[createPosts] response is not ok. status', {
                 message: res.status,
             });
+            logger.info('res: ', { message: res });
+            logger.info('newPost: ', { message: newPost });
             throw new Error('createPosts error');
         }
+        revalidateTag(type === POST_TYPE.QUEST ? POST_TYPE.QUEST : POST_TYPE.SB);
     } catch (error) {
         logger.error('createPosts error', { message: error });
         return null;
     }
 
-    revalidateTag('all-posts');
     redirect('/');
+}
+
+async function formQuest(accessToken: string, newPost: PostType) {
+    let res: Response | null = null;
+    try {
+        res = await fetch(`${backendURL}/quests`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(newPost),
+        });
+        return res;
+    } catch (error) {
+        logger.error('formQuest error', { message: error });
+        throw new Error('formQuest error');
+    }
+}
+
+async function formSubmissions(accessToken: string, newPost: PostType) {
+    let res: Response;
+    try {
+        res = await fetch(`${backendURL}/sbs`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(newPost),
+        });
+
+        return res;
+    } catch (error) {
+        logger.error('formSubmissions error', { message: error });
+        throw new Error('formSubmissions error');
+    }
 }
---
+++ front/src/utils/cloudinary/lib.ts
@@ -0,0 +1,165 @@
+import 'server-only';
+import { v2 as cloudinary } from 'cloudinary';
+import logger from '@/utils/log/logger';
+import { UploadApiResponse, type UploadApiErrorResponse } from 'cloudinary';
+
+const configCld = {
+    secure: true,
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+};
+
+cloudinary.config({
+    ...configCld,
+});
+
+enum UploadType {
+    QUEST = 'quests',
+    SB = 'submissions',
+    USER_PROFILE = 'profiles',
+}
+
+// Public function
+export function validateImage(imageFile: File) {
+    /*TODO 
+        - image 유효성 확인 로직 구현
+    */
+    if (imageFile && imageFile.size > 0) return true;
+
+    return false;
+}
+
+export async function uploadQuestImage(file: File, authorEmail: string) {
+    return uploadImage(file, authorEmail, UploadType.QUEST);
+}
+
+export async function uploadSubmissionImage(file: File, authorEmail: string) {
+    return uploadImage(file, authorEmail, UploadType.SB);
+}
+
+export async function uploadUserProfileImage(file: File, authorEmail: string) {
+    return uploadImage(file, authorEmail, UploadType.USER_PROFILE);
+}
+
+// Internal function
+
+async function uploadImage(ImageFile: File, authorEmail: string, type: UploadType) {
+    try {
+        const arrayBuffer = await ImageFile.arrayBuffer();
+        const buffer = new Uint8Array(arrayBuffer);
+
+        const timeStamp = Date.now();
+        const fileName = `[${timeStamp}]_${ImageFile.name}_${authorEmail}`;
+        const folder = await createSubFolder(type);
+
+        let ret: UploadApiResponse | null = null;
+        /*TODO
+            - ref : https://cloudinary.com/documentation/image_upload_api_reference#upload_optional_parameters
+        */
+        // Large File upload logic.
+        ret = await new Promise((resolve, reject) => {
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        display_name: `${fileName}`,
+                        asset_folder: `${folder}`,
+                        use_asset_folder_as_public_id_prefix: true,
+                    },
+                    (error, result) => {
+                        if (error) {
+                            reject(error as UploadApiErrorResponse);
+                            return;
+                        }
+                        resolve(result as UploadApiResponse);
+                    },
+                )
+                .end(buffer);
+        })
+            .then((uploadResult) => {
+                return uploadResult as UploadApiResponse;
+            })
+            .catch((error) => {
+                logger.error('uploadStream error', { message: error });
+                throw new Error('uploadStream error');
+            });
+        logger.info('ret: ', ret);
+
+        return ret.public_id;
+    } catch (error) {
+        logger.error('cloudinary.config(): ', cloudinary.config());
+        logger.error('uploadThumbnail error', { message: error });
+        return null;
+    }
+}
+
+async function createSubFolder(type: UploadType) {
+    const time = new Date();
+    let parentFolder: string | null = null;
+    switch (type) {
+        case UploadType.QUEST:
+            parentFolder = 'quests';
+            break;
+        case UploadType.SB:
+            parentFolder = 'submissions';
+            break;
+        case UploadType.USER_PROFILE:
+            parentFolder = 'profiles';
+            break;
+        default:
+            break;
+    }
+
+    if (!parentFolder) {
+        logger.error('createSubFolder error');
+        throw new Error('createSubFolder error');
+    }
+
+    const createYear = `[${time.getFullYear()}]`;
+    logger.info('createYear: ', createYear);
+    const folderName = `${parentFolder}/${createYear}`;
+    await cloudinary.api
+        .create_folder(folderName)
+        .then((result) => logger.info('create_folder success', { message: result }))
+        .catch((error) => {
+            logger.error('cloudinary create_folder error', { message: error });
+            throw new Error('createQuestSubFolder error');
+        });
+
+    logger.info('createQuestSubFolder success', folderName);
+    return folderName;
+}
+
+export async function uploadSmallFile(formData: FormData, folder: string) {
+    let ret = null;
+    try {
+        const base64String = formData.get('Base64') as string;
+        await cloudinary.api.create_folder(folder).catch((error) => {
+            logger.error('cloudinary create_folder error', { message: error });
+            throw new Error('[uploadSmallFile] create_folder error');
+        });
+        await cloudinary.uploader
+            .upload(base64String, {
+                use_filename: true,
+                asset_folder: `${folder}`,
+                use_asset_folder_as_public_id_prefix: true,
+            })
+            .then((uploadResult) => {
+                ret = uploadResult as UploadApiResponse;
+                logger.info('Buffer uploadResult: ', uploadResult);
+                logger.info('public_id: ', ret.public_id);
+            })
+            .catch((error) => {
+                logger.error('upload error', { message: error });
+                throw new Error('upload error');
+            });
+
+        logger.info('ret: ', { message: ret });
+
+        return ret;
+    } catch (error) {
+        logger.error('cloudinary.config(): ', cloudinary.config());
+        logger.error('uploadThumbnail error', { message: error });
+        return null;
+    }
+}
