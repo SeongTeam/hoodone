e31f75d54da8930d94f1a55d691a848f16ca8521
armcortex3445 <wlghks5966@naver.com>
2024-07-08 15:14:57 +0900

    <fix>[front] Fix post taglist input logic
    - Add logic for showing tag segment
    - fix logic that mutates new post taglist
    - fix mutating tag list

--- front/src/components/posts/create/postFormat/textInput.tsx
+++ front/src/components/posts/create/postFormat/textInput.tsx
@@ -1,9 +1,10 @@
 "use client"
 import React from 'react';
-import { Box, Button, Container, Flex, Input, Stack, Textarea, VStack } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Input, Stack, Textarea, VStack, Tag, TagRightIcon, TagLabel } from '@chakra-ui/react';
 import TextEditor from './subComponent/textEditor';
 import { customColors } from '@/utils/chakra/customColors';
 import { NewPostFormType } from '@/type/postType';
+import { CloseIcon } from '@chakra-ui/icons';
 
 /* TODO
 - Input 스타일 css 파일로 적용하기
@@ -29,6 +30,7 @@ const TextInput: React.FC<TextInputProps> = ({
     const bg = customColors.white;
     const inputBorderColor = customColors.shadeLavender[300];
     const focusBorderColor = customColors.skyBlue[100];
+    const [inserting , setInserting] = React.useState<boolean>(false);
 
     const breakpoints = {
         base: '0em', // 0px
@@ -50,6 +52,42 @@ const TextInput: React.FC<TextInputProps> = ({
         }));
 
     } 
+    const [tagSegment, setTagSegment] = React.useState<string>('');
+
+    const removeTag = (target : string) => {
+        setPost(prevPost => ({
+            ...prevPost,
+            tags : prevPost.tags.filter(tag => tag !== target)
+        }));
+    }
+
+    const handleTagChange = ( event : React.ChangeEvent<HTMLInputElement> ) => {
+
+        const segment = event.target.value;
+        const lastCh = segment[segment.length - 1];
+
+
+        switch(lastCh) {
+            case '\n':
+            case ' ':
+                const newTag = segment.trim();
+                if(newTag && !post.tags.includes(newTag)) {
+                    setPost(prevPost => ({
+                        ...prevPost,
+                        tags : [...prevPost.tags, newTag]
+                    }));
+                    setTagSegment('');
+                }
+                else{
+                    setTagSegment('');
+                }
+                break;
+            default:
+                setTagSegment(segment);
+                break;
+        }
+
+    }
 
 
     return (
@@ -84,18 +122,45 @@ const TextInput: React.FC<TextInputProps> = ({
                     value={post.content}
                 ></Textarea>
 
-                <Input
-                    border={`1px solid ${inputBorderColor}`}
-                    focusBorderColor={focusBorderColor}
-                    borderRadius="8px"
-                    color={fontColor}
-                    name="tags"
-                    value={post.tags}
-                    /**TODO Tage onChange logic 추가하기 */
-                    onChange={handleChange}
-                    placeholder={tagsPlaceHolder}
-                    bg={bg}
-                />
+                <Box
+                    w= "100%"
+                    id = "tagArea"
+                >
+                    {
+                        post.tags && Array.isArray(post.tags) &&post.tags.map( (tag, index) => (
+                            <Tag
+                                key = {tag + index}
+                                variant="postTag"
+                                mr="5px"
+                                mb="5px"
+                                onClick={() => removeTag(tag)}
+                                cursor="pointer"
+                                _hover={{
+                                    bg : customColors.gray[300]
+                                }}
+                            >
+                                <TagLabel>#{tag}</TagLabel>
+                                <TagRightIcon  
+                                    boxSize="8px" 
+                                    as={CloseIcon} 
+                                    />
+                            </Tag>
+                        ))
+                    }
+                    <Box h="10px"/>
+                    <Input
+                        border={`1px solid ${inputBorderColor}`}
+                        focusBorderColor={focusBorderColor}
+                        borderRadius="8px"
+                        color={fontColor}
+                        name="tag"
+                        value={tagSegment}
+                        /**TODO Tage onChange logic 추가하기 */
+                        onChange={handleTagChange}
+                        placeholder={tagsPlaceHolder}
+                        bg={bg}
+                    />
+                </Box>
             </VStack>
         </Box>
     );
