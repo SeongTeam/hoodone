152023c4611f20089aca9fd944440dde75ad179a
armcortex3445 <wlghks5966@naver.com>
2024-07-09 17:42:36 +0900

    <feature>[front] Add post edit interface and UI
    - Modify <createPosts/> for reusing during editing post and creating
    - Add server-action logic for editing post
    - Modify createPosts server-action for more readability and resuability

--- front/src/server-actions/postsActions.ts
+++ front/src/server-actions/postsActions.ts
@@ -24,15 +24,12 @@ ref : https://www.youtube.com/watch?v=5L5YoFm1obk
 
 const backendURL = process.env.BACKEND_URL;
 
-export async function createPosts(formData: FormData) {
+export async function createPosts(formData: FormData, editPostId?: number) {
     /*TODO 
-    - tag : string[] 타입 전송 구현하기
     - upload 실패 예외처리 로직 구현
       - 발생한 에러 명확한 정보 logger로 출력
       - 예외 상황에 따른 시나리오 고려 및 구현
-    - 테스트 진행하기
-        - 테스트 진행을 위해서 login 로직필요
-        - 
+
   */
     const postDtoString = formData.get(NEW_POST_FORMAT.POST_DTO);
     const imageFile = formData.get(NEW_POST_FORMAT.IMAGE) as File;
@@ -56,48 +53,34 @@ export async function createPosts(formData: FormData) {
         const accessToken = await validateAuth();
         const type = postDto.type;
 
-        if (validateImage(imageFile)) {
-            let publicId: string | null = null;
-            const userInfo = await getUserBasicInfo();
-            if (!userInfo) {
-                logger.error('[createPosts] getUserBasicInfo fail ', { userInfo });
-                throw new Error('[createPosts] getUserBasicInfo error');
-            }
+        await uploadImage(imageFile, type, newPost);
 
+        if (editPostId) {
             switch (type) {
                 case POST_TYPE.QUEST:
-                    publicId = await uploadQuestImage(imageFile, userInfo.email);
+                    res = await editQuest(accessToken, editPostId, newPost);
                     break;
                 case POST_TYPE.SB:
-                    publicId = await uploadSubmissionImage(imageFile, userInfo.email);
+                    res = await editSubmission(accessToken, editPostId, newPost);
                     break;
                 default:
+                    logger.error('[createPosts] edit fail. POST_TYPE is invalid. type = ', type);
                     throw new Error('[createPosts] POST_TYPE error');
                     break;
             }
-
-            if (publicId) newPost.cloudinaryPublicId = publicId;
-            else throw new Error('[createPosts] uploadQuestImage error');
-        } else if (!imageFile) {
-            if (type === POST_TYPE.QUEST)
-                newPost.cloudinaryPublicId = cloudinaryTempData.defaultQuestPublicId;
-            else newPost.cloudinaryPublicId = cloudinaryTempData.defaultSBPublicId;
-        }
-        /*TODO
-        - token get 실패시, 홈이동 > 로그인 창 오픈. 
-        */
-
-        switch (type) {
-            case POST_TYPE.QUEST:
-                res = await formQuest(accessToken, newPost);
-                break;
-            case POST_TYPE.SB:
-                res = await formSubmissions(accessToken, newPost);
-                break;
-            default:
-                logger.error('[createPosts] POST_TYPE is invalid. type = ', type);
-                throw new Error('[createPosts] POST_TYPE error');
-                break;
+        } else {
+            switch (type) {
+                case POST_TYPE.QUEST:
+                    res = await formQuest(accessToken, newPost);
+                    break;
+                case POST_TYPE.SB:
+                    res = await formSubmissions(accessToken, newPost);
+                    break;
+                default:
+                    logger.error('[createPosts] create fail. POST_TYPE is invalid. type = ', type);
+                    throw new Error('[createPosts] POST_TYPE error');
+                    break;
+            }
         }
 
         if (!res.ok) {
@@ -151,5 +134,71 @@ async function formSubmissions(accessToken: string, newPost: PostType) {
     } catch (error) {
         logger.error('formSubmissions error', { message: error });
         throw new Error('formSubmissions error');
+    }
+}
+
+async function editQuest(accessToken: string, questId: number, newPost: PostType) {
+    let res: Response | null = null;
+    try {
+        res = await fetch(`${backendURL}/quests/${questId}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(newPost),
+        });
+        return res;
+    } catch (error) {
+        logger.error('editQuest error', { message: error });
+        throw new Error('editQuest error');
+    }
+}
+
+async function editSubmission(accessToken: string, sbId: number, newPost: PostType) {
+    let res: Response | null = null;
+    try {
+        res = await fetch(`${backendURL}/sbs/${sbId}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(newPost),
+        });
+        return res;
+    } catch (error) {
+        logger.error('editSubmission error', { message: error });
+        throw new Error('editSubmission error');
+    }
+}
+
+async function uploadImage(imageFile: File, type: POST_TYPE, newPost: PostType) {
+    if (validateImage(imageFile)) {
+        let publicId: string | null = null;
+        const userInfo = await getUserBasicInfo();
+        if (!userInfo) {
+            logger.error('[createPosts] getUserBasicInfo fail ', { userInfo });
+            throw new Error('[createPosts] getUserBasicInfo error');
+        }
+
+        switch (type) {
+            case POST_TYPE.QUEST:
+                publicId = await uploadQuestImage(imageFile, userInfo.email);
+                break;
+            case POST_TYPE.SB:
+                publicId = await uploadSubmissionImage(imageFile, userInfo.email);
+                break;
+            default:
+                throw new Error('[createPosts] POST_TYPE error');
+                break;
+        }
+
+        if (publicId) newPost.cloudinaryPublicId = publicId;
+        else throw new Error('[createPosts] uploadQuestImage error');
+    } else if (!imageFile) {
+        if (type === POST_TYPE.QUEST)
+            newPost.cloudinaryPublicId = cloudinaryTempData.defaultQuestPublicId;
+        else newPost.cloudinaryPublicId = cloudinaryTempData.defaultSBPublicId;
     }
 }
