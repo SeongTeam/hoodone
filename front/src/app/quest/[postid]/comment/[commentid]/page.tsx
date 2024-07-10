6813dd7a260f97e2f76768156564cbd02a238685
armcortex3445 <wlghks5966@naver.com>
2024-07-10 12:55:07 +0900

    <fix>[front] Fix post reply page UI
    - Fix post reply page UI
    - delete not using component

--- front/src/app/quest/[postid]/comment/[commentid]/page.tsx
+++ front/src/app/quest/[postid]/comment/[commentid]/page.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from "next";
 import { PostFetchService } from "@/lib/server-only/postLib";
 import { POST_TYPE, PostType } from '@/type/postType';
-import { Flex } from "@chakra-ui/react";
+import { Grid,SimpleGrid,Spacer,Text, VStack } from "@chakra-ui/react";
 import logger from "@/utils/log/logger";
-import Post from "@/components/posts/view/server-component/post";
-
+import RuleCard from "@/components/posts/card/RuleCard";
+import DetailPostForm from "@/components/posts/detail/detailPostForm";
+import PostSlider from "@/components/_global/slider/postSlider";
+import MiniPostCard from "@/components/posts/card/MiniPostCard";
+import { questPostRuleText } from "@/components/posts/card/const/rule_card_texts";
+import { customColors } from "@/utils/chakra/customColors";
+import CommentArea from "@/components/comment/server-component/commentArea";
 type PostPageWithReplyProps = {
     params: {
         postid: string,
@@ -20,8 +25,13 @@ const PostPageWithReply : NextPage<PostPageWithReplyProps> = async (
     searchParams
 }
 ) => {
+    const inputBorderColor = customColors.shadeLavender[300];
+
     const postService = new PostFetchService(POST_TYPE.QUEST);
     const post : PostType | null = await postService.getPostByID(params.postid, parseInt(searchParams.index));
+    
+    const allPosts: PostType[] | null = await postService.getCachedPaginatedPosts(1);
+
     logger.info('#PostPageWithReply Rendered', { message: JSON.stringify(params) });
     if(!post ) {
         logger.error(`post${params.postid} not found`);
@@ -29,9 +39,49 @@ const PostPageWithReply : NextPage<PostPageWithReplyProps> = async (
     }
 
     return (
-        <Flex w={"full"} flexDir={"column"}>
-            <Post post={post} rootCommentID={parseInt(params.commentid)}/>
-        </Flex>
+        <SimpleGrid columns={{ sm: 1, md: 1 }} spacing="24px">
+        <Grid
+            templateColumns={{ md: '1fr', lg: '1fr', xl: '3fr 1fr' }}
+            templateRows={{ sm: '1fr auto', md: '2fr' }}
+            gap="24px"
+        >
+            <RuleCard
+                title="Quest"
+                cardTexts={questPostRuleText}
+                displayOption={{ sm: 'block', md: 'block', lg: 'block', xl: 'none' }}
+            ></RuleCard>
+            <VStack
+                w="100%"
+                minW="300px"
+                bg="FFFFFF"
+                align="left"
+                px={{ sm: '0', md: '14px', lg: '24px' }}
+                py="24px"
+                borderRadius="15px"
+                border={`1px solid ${inputBorderColor}`}
+            >
+                <DetailPostForm type= {POST_TYPE.QUEST} post={post}></DetailPostForm>
+
+                <Spacer h="6px" />
+
+                <Text fontSize="1.4em"> Submission</Text>
+                <PostSlider sliderName="sbsPostsOnDetail" hight="190px">
+                    {allPosts?.map((post, index) => (
+                        <MiniPostCard key={index} index={index} post={post} />
+                    ))}
+                </PostSlider>
+
+                <Spacer h="26px" />
+
+                <CommentArea postType={POST_TYPE.QUEST} postID={post.id} rootCommentID={parseInt(params.commentid)}></CommentArea>
+            </VStack>
+            <RuleCard
+                title="Quest"
+                cardTexts={questPostRuleText}
+                displayOption={{ md: 'none', lg: 'none', xl: 'block' }}
+            ></RuleCard>
+        </Grid>
+    </SimpleGrid>
     );
 
 
