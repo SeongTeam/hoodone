73fd492407a2b28a8acd9948675019f81b1b6a70
armcortex3445 <wlghks5966@naver.com>
2024-07-21 18:26:18 +0900

    <fix>[front] fix slider component responsive error
    - fix error that slider component width is not fitted with mobile device

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    logging : {
+        // cache clear : ctrl + shift + R  
+        fetches: {
+            fullUrl: true,
+        }
+    }
 };
 
 export default nextConfig;
